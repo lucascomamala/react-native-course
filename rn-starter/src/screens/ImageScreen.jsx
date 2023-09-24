5fba04b225690b854f0c95f0a52465f550a85279
Lucas <lucascomamala@gmail.com>
2023-09-24 03:43:28 -0600

    32 - Exercise Solution

---
+++ rn-starter/src/screens/ImageScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const ImagesScreen = () => {
+  return (
+    <Text>ImagesScreen</Text>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ImagesScreen
