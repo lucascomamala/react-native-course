96be524b62afc231ff3b8b5cc8e0b68e01e9e55e
Lucas <lucascomamala@gmail.com>
2023-09-24 00:51:03 -0600

    22 - Solving the Key Issue

--- rn-starter/src/screens/ListScreen.jsx
+++ rn-starter/src/screens/ListScreen.jsx
@@ -8,10 +8,11 @@ const ListScreen = () => {
     { name: 'Friend #4' },
     { name: 'Friend #5' },
     { name: 'Friend #6' },
-    { name: 'Friend #7' },
+    { name: 'Frievvnd #7' },
   ]
   return (
     <FlatList
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
         return <Text>{item.name}</Text>
