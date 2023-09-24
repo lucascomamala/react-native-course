4c97ce69767b2024cafa774b756f21d941133421
Lucas <lucascomamala@gmail.com>
2023-09-24 14:43:21 -0600

    41 - Screen Boilerplate

---
+++ rn-starter/src/screens/CounterScreen.jsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>
+        CounterScreen
+      </Text>
+    </View >
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default CounterScreen
--- rn-starter/src/screens/HomeScreen.jsx
+++ rn-starter/src/screens/HomeScreen.jsx
@@ -15,6 +15,10 @@ const HomeScreen = ({ navigation }) => {
       title='Go to Image Demo'
       onPress={() => navigation.navigate('Image')}
     />
+    <Button
+      title='Go to Counter Demo'
+      onPress={() => navigation.navigate('Counter')}
+    />
   </View>
 };
 
