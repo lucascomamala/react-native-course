1a3d21cda6d656f1d0417787acfcd8bffebe44d0
Lucas <lucascomamala@gmail.com>
2023-09-24 15:07:37 -0600

    44 - App Overview

---
+++ rn-starter/src/screens/ColorScreen.jsx
@@ -0,0 +1,17 @@
+import { View, StyleSheet, Button } from 'react-native'
+import { useState } from 'react'
+
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Button
+        title='Add a Color'
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ColorScreen
