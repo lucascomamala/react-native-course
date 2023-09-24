f486674e4d6ae99d903aef401174c4a4f65c10a4
Lucas <lucascomamala@gmail.com>
2023-09-23 21:59:06 -0600

    11 - Overview of React Components

---
+++ rn-starter/src/screens/ComponentsScreen.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+import React from 'react'
+
+const ComponentsScreen = () => {
+  return (
+    <Text style={styles.textStyle}>ComponentsScreen</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30
+  }
+})
+
+export default ComponentsScreen
