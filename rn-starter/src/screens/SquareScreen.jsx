456526313970f0890152e45104962488cdf8f870
Lucas <lucascomamala@gmail.com>
2023-09-24 18:08:30 -0600

    48 - App Overview

---
+++ rn-starter/src/screens/SquareScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+import { useState } from 'react'
+
+const SquareScreen = () => {
+  return (
+    <Text>SquareScreen</Text>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default SquareScreen
