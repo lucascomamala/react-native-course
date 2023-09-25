283b77bb94ca494d3ee0ee6d20629d894677458f
Lucas <lucascomamala@gmail.com>
2023-09-24 23:10:50 -0600

    62 - Handling Text Input

---
+++ rn-starter/src/screens/TextScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>
+        TextScreen
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default TextScreen
