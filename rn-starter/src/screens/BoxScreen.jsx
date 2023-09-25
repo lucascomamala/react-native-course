825f23a52c55ace6cd0ff9bf03ea836cd74da9dc
Lucas <lucascomamala@gmail.com>
2023-09-24 23:30:06 -0600

    69 - Layout with React Native

---
+++ rn-starter/src/screens/BoxScreen.jsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const BoxScreen = () => {
+  return (
+    <View>
+      <Text>
+        BoxScreen
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default BoxScreen
