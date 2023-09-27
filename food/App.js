97f291539ecf329ac530066fbcd5f408dd36a9ac
Lucas <lucascomamala@gmail.com>
2023-09-27 17:10:54 -0600

    115 - Reminder on Navigation

--- food/App.js
+++ food/App.js
@@ -3,14 +3,18 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { StyleSheet, Text, View } from 'react-native'
 
 import SearchScreen from './src/screens/SearchScreen'
+import RestaurantDetailScreen from './src/screens/RestaurantDetailScreen'
 
-const navigator = createStackNavigator({
-  Search: SearchScreen,
-}, {
-  initialRouteName: 'Search',
-  defaultNavigationOptions: {
-    title: 'Business Search',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+    RestaurantShow: RestaurantDetailScreen,
   },
-})
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search',
+    },
+  })
 
 export default createAppContainer(navigator)
---
+++ food/src/screens/RestaurantDetailScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+
+const RestaurantDetailScreen = () => {
+  return (
+    <View>
+      <Text>Restaurant Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default RestaurantDetailScreen
