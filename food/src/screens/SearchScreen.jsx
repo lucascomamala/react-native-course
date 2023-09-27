93cddf368d99541c89d058ab7a40c440b00444c7
Lucas <lucascomamala@gmail.com>
2023-09-27 13:23:20 -0600

    100 - Error Handling

--- food/src/screens/SearchScreen.jsx
+++ food/src/screens/SearchScreen.jsx
@@ -8,16 +8,21 @@ import SearchBar from '../components/SearchBar'
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      }      
-    })
-    setResults(response.data.businesses)
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        }
+      })
+      setResults(response.data.businesses)
+    } catch (error) {
+      setErrorMessage('Something went wrong')
+    }
   }
 
   return (
@@ -27,9 +32,7 @@ const SearchScreen = () => {
         onTermChange={newTerm => setTerm(newTerm)}
         onTermSubmit={searchApi}
       />
-      <Text>
-        SearchScreen
-      </Text>
+      {errorMessage && <Text>{errorMessage}</Text>}
       <Text>
         We have found {results.length} results
       </Text>
