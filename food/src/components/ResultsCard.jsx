0cf4c5ca8a7d456fc2f5ce5859711cb56b0d922b
Lucas <lucascomamala@gmail.com>
2023-09-27 16:56:53 -0600

    109 - Showing Additional Info

--- food/src/components/ResultsCard.jsx
+++ food/src/components/ResultsCard.jsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
 const ResultsCard = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image
+        source={{ uri: result.image_url }}
+        style={styles.image}
+      />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  name: {
+    fontWeight: 'bold',
+  }
+})
 
 export default ResultsCard
