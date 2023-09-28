952b1b7dcd3e427bd78bf10dec7ae1666d65b2f7
Lucas <lucascomamala@gmail.com>
2023-09-28 01:30:30 -0600

    139 - A Bit of Styling

--- blog/src/screens/IndexScreen.jsx
+++ blog/src/screens/IndexScreen.jsx
@@ -1,5 +1,6 @@
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import { useContext } from 'react'
+import { FontAwesome } from '@expo/vector-icons'
 
 import { Context as BlogContext } from '../context/BlogContext'
 
@@ -8,7 +9,6 @@ const IndexScreen = () => {
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button
         title="Add Post"
         onPress={addBlogPost}
@@ -17,13 +17,31 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>
+          return <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <FontAwesome style={styles.icon} name="trash-o" />
+          </View>
         }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+})
 
 export default IndexScreen
