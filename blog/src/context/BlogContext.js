bea069e926b230728bf2a5039b35721c6304ea27
Lucas <lucascomamala@gmail.com>
2023-09-28 00:07:36 -0600

    130 - Adding Context

---
+++ blog/src/context/BlogContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react"
+
+const BlogContext = createContext()
+
+export const BlogProvider = ({ children }) => {
+  return (
+    <BlogContext.Provider value={5}>
+      {children}
+    </BlogContext.Provider>
+  )
+}
