fecb618679f2e2902e9c742b71aaa3bdea32f0c9
Lucas <lucascomamala@gmail.com>
2023-09-28 17:15:59 -0600

    144 - Adding a Creation Screen

---
+++ blog/src/screens/CreateScreen.jsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import { useContext } from 'react'
+
+import { Context as BlogContext } from '../context/BlogContext'
+
+const CreateScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default CreateScreen
