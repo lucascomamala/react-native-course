19f47c291560213757eb7ffd05a712c211e291f8
Lucas <lucascomamala@gmail.com>
2023-09-27 22:54:51 -0600

    Update app.js

--- food/App.js
+++ food/App.js
@@ -1,6 +1,5 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { StyleSheet, Text, View } from 'react-native'
 
 import SearchScreen from './src/screens/SearchScreen'
 import RestaurantDetailScreen from './src/screens/RestaurantDetailScreen'
