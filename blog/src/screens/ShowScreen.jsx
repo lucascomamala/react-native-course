eadcbbe8d830a4e17fb8e10036b3bf4b9d2a72c9
Lucas <lucascomamala@gmail.com>
2023-09-28 17:15:20 -0600

    143 - Retrieving Single Posts

--- blog/src/screens/ShowScreen.jsx
+++ blog/src/screens/ShowScreen.jsx
@@ -1,15 +1,21 @@
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { useContext } from 'react'
 
 import { Context as BlogContext } from '../context/BlogContext'
 
-const IndexScreen = () => {
+const ShowScreen = ({navigation}) => {
+  const id = navigation.getParam('id')
+  const { state } = useContext(BlogContext)
+
+  const blogPost = state.find((blogPost) => blogPost.id === id)
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({})
 
-export default IndexScreen
+export default ShowScreen
