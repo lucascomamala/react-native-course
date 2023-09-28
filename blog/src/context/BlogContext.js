765c0aedf9ebd10cc7fc11917708d637889475e3
Lucas <lucascomamala@gmail.com>
2023-09-28 00:13:42 -0600

    132 - Rendering a List of Posts

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,8 +3,13 @@ import { createContext } from "react"
 const BlogContext = createContext()
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' }
+  ]
+
   return (
-    <BlogContext.Provider value={5}>
+    <BlogContext.Provider value={blogPosts}>
       {children}
     </BlogContext.Provider>
   )
