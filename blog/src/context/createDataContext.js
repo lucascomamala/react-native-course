8ac42793d22ce6315b8a4de93e7888a8ebe72e43
Lucas <lucascomamala@gmail.com>
2023-09-28 01:10:08 -0600

    137 - Automating Context Creation

---
+++ blog/src/context/createDataContext.js
@@ -0,0 +1,17 @@
+import { useReducer, createContext } from "react"
+
+export default (reducer, actions, initialState) => {
+  const Context = createContext()
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+      <Context.Provider value={{ state }}>
+        {children}
+      </Context.Provider>
+    )
+  }
+
+  return { Context, Provider }
+}
