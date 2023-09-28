2d2faf7717b3cbe87ca07b7bca2c08cdbb486e06
Lucas <lucascomamala@gmail.com>
2023-09-28 17:29:30 -0600

    148 - Saving a New Post

--- blog/src/screens/CreateScreen.jsx
+++ blog/src/screens/CreateScreen.jsx
@@ -6,6 +6,7 @@ import { Context as BlogContext } from '../context/BlogContext'
 const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const { addBlogPost } = useContext(BlogContext)
 
   return (
     <View>
@@ -13,7 +14,12 @@ const CreateScreen = ({ navigation }) => {
       <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
       <Text style={styles.label}>Enter Content:</Text>
       <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
-      <Button title="Publish" />
+      <Button
+        title="Publish"
+        onPress={() => {
+          addBlogPost(title, content)
+        }}
+      />
     </View>
   )
 }
