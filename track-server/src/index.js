2383fa44615b599568246fdadf2c6cae540fbd77
Lucas <lucascomamala@gmail.com>
2023-09-29 04:30:11 -0600

    180 - Understanding the Signup Process

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
+app.use(authRoutes)
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.rk83yxw.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoUri)
