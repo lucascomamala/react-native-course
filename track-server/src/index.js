1e21ea2fcc51535d34f2c877382d5a2811951743
Lucas <lucascomamala@gmail.com>
2023-09-29 03:26:54 -0600

    178 - Connecting to MongoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.rk83yxw.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoUri)
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err)
+})
 
 app.get('/', (req, res) => {
   res.send('Hi there')
