d5e00288f38753d8c59cba2256e5195513e51250
Lucas <lucascomamala@gmail.com>
2023-09-28 18:25:49 -0600

    154 - Extracting Form Logic

--- blog/src/screens/CreateScreen.jsx
+++ blog/src/screens/CreateScreen.jsx
@@ -1,45 +1,19 @@
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
-import { useContext, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { useContext } from 'react'
 
 import { Context as BlogContext } from '../context/BlogContext'
+import BlogPostForm from '../conmponents/BlogPostForm'
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
   const { addBlogPost } = useContext(BlogContext)
 
   return (
-    <View>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
-      <Button
-        title="Publish"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate('Index')
-          })
-        }}
-      />
-    </View>
+    <BlogPostForm />
   )
 }
 
 const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  }
+
 })
 
 export default CreateScreen
--- blog/src/screens/EditScreen.jsx
+++ blog/src/screens/EditScreen.jsx
@@ -1,42 +1,23 @@
-import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
-import { useContext, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { useContext } from 'react'
 
 import { Context as BlogContext } from '../context/BlogContext'
+import BlogPostForm from '../conmponents/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(BlogContext)
   const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
 
-  const [title, setTitle] = useState(blogPost.title)
-  const [content, setContent] = useState(blogPost.content)
-
   return (
-    <View>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
-      <Text style={styles.label}>Edit Content:</Text>
-      <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
-      <Button
-        title="Save"
-      />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        console.log(title, content)
+      }}
+    />
   )
 }
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  }
-})
+const styles = StyleSheet.create({})
 
 export default EditScreen
